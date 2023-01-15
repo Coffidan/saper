ab9739770487c3d27d7d2bb9ee7bd405ae112295
Toyua <117737840+Coffidan@users.noreply.github.com>
2023-01-15 16:00:29 +0300

    Add files via upload

---
+++ Global.h
@@ -0,0 +1,20 @@
+#pragma once
+
+constexpr unsigned char CELL_SIZE = 16;
+
+constexpr unsigned char COLUMNS = 8;
+
+constexpr unsigned char EFFECT_DURATION = 16;
+
+constexpr unsigned char FONT_HEIGHT = 16;
+
+constexpr unsigned char FONT_WIDTH = 8;
+
+constexpr unsigned char ROWS = 8;
+
+constexpr unsigned char SCREEN_RESIZE = 4;
+
+
+constexpr unsigned short FRAME_DURATION = 16667;
+
+constexpr unsigned short MINES = 8;
